cf8653a88b403295fb7a2918ff6269d686e7b85f
Luis Rovirosa Mairlot <luis@codium.team>
2023-03-17 19:23:40 +0100

    refactor rename tests

--- kotlin/src/test/kotlin/ExampleTest.kt
+++ kotlin/src/test/kotlin/ExampleTest.kt
@@ -1,31 +1,29 @@
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestFactory
 
 class ExampleTest {
 
     @Test
-    fun sizeTest() {
+    fun `size of an empty pile is zero`() {
         val miPila = Pila()
         Assertions.assertEquals(0, miPila.size())
     }
 
     @Test
-    fun emptyCheckTest(){
+    fun `empty pile returns empty check`() {
         val miPila = Pila()
-        Assertions.assertEquals(true,miPila.emptyCheck())
+        Assertions.assertTrue(miPila.emptyCheck())
     }
 
     @Test
-    fun pushTest(){
+    fun `size of a pile with one element is one`() {
         val miPila = Pila()
         miPila.push(1)
-        Assertions.assertEquals(1,miPila.size())
+        Assertions.assertEquals(1, miPila.size())
     }
 
     @Test
-    fun peekTest(){
+    fun `peek of an empty pile is null`() {
         val miPila = Pila()
         Assertions.assertNull(miPila.peek())
     }
